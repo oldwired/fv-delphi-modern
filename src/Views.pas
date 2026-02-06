7a20ac47e47c98385939028c0586b545fa7ad926
Andreas Pfau <a.pfau@pfau.de>
2026-02-06 22:00:32 +0100

    fixed the scrollbars drawing

--- src/Views.pas
+++ src/Views.pas
@@ -1129,7 +1129,6 @@ var
   ClipRight: Integer;
   ClipLeft, ClipTop, ClipBottom: Integer;
   XOffset: Integer;
-  FrameInset: Integer;
   ScreenOffset: Integer;
   DrawBuf: PDrawBuffer;
   Cell: TDrawCell;
@@ -1152,19 +1151,10 @@ begin
         { Walk up the owner chain, checking clip at each level }
         V := Owner;
         while V <> nil do begin
-          { Inset clip by 1 for framed views (windows) - but not for the frame itself }
-          { Frame child is detected by: view is at (0,0) with same size as owner }
-          { Only apply inset if owner has ofFramed flag set }
-          FrameInset := 0;
-          if (V = Owner) and ((V.Options and ofFramed) <> 0) and
-             ((Origin.X <> 0) or (Origin.Y <> 0) or
-             (Size.X <> V.Size.X) or (Size.Y <> V.Size.Y)) then
-            FrameInset := 1;
-
-          ClipTop := TGroup(V).Clip.A.Y + FrameInset;
-          ClipBottom := TGroup(V).Clip.B.Y - FrameInset;
-          ClipLeft := TGroup(V).Clip.A.X + FrameInset;
-          ClipRight := TGroup(V).Clip.B.X - FrameInset;
+          ClipTop := TGroup(V).Clip.A.Y;
+          ClipBottom := TGroup(V).Clip.B.Y;
+          ClipLeft := TGroup(V).Clip.A.X;
+          ClipRight := TGroup(V).Clip.B.X;
 
           { Check vertical clipping }
           if (GY < ClipTop) or (GY >= ClipBottom) then begin
